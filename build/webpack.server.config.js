55c5f440db939864ca7b3edb184441eaa7fad031
lanhaoxiang <lanhaoxiang@qq.com>
2018-11-22 16:29:53 +0800

    feat: upgrade preset

--- build/webpack.server.config.js
+++ build/webpack.server.config.js
@@ -42,7 +42,7 @@ module.exports = {
                 ],
                 query: {
                     plugins: ['transform-runtime'],
-                    presets: ['es2015', 'stage-2'],
+                    presets: ['env', 'stage-2'],
                 }
             },
             {
