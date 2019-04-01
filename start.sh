b15ad156d90beb88d67f2110ee12d2413315791b
lanhaoxiang <lanhaoxiang@qq.com>
2019-04-01 14:31:21 +0800

    chore: update start script, will not sync blocks locally

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 pm2 stop gxb-explorer
-SYNC=1 NODE_ENV=production pm2 start server-dist/index.js --name gxb-explorer
+NODE_ENV=production pm2 start server-dist/index.js --name gxb-explorer
