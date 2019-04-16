0cb150489a01847961c6912c183840255e760875
lanhaoxiang <lanhaoxiang@qq.com>
2019-04-16 18:06:21 +0800

    chore: adjust get_table_rows_ex upper_bound

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -7,15 +7,16 @@
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             <span class="fa fa-fw fa-address-card"></span> {{$t('account.basic.title')}}
-                            
-                            <a v-if="network.chainId =='4f7d07969c446f8342033acb3ab2ae5044cbe0fde93db02de75bd17fa8fd84b8'"  class="pull-right"
+
+                            <a v-if="network.chainId =='4f7d07969c446f8342033acb3ab2ae5044cbe0fde93db02de75bd17fa8fd84b8'"
+                               class="pull-right"
                                :href="'https://wallet.gxb.io/#/account/'+account_info.name+'/overview'"
                                target="_blank">{{$t('account.basic.more')}}</a>
 
                             <a v-else class="pull-right"
                                :href="'https://testnet.wallet.gxchain.org/#/account/'+account_info.name+'/overview'"
                                target="_blank">{{$t('account.basic.more')}}</a>
-                               
+
                         </div>
                         <div class="panel-body no-padding">
                             <div class="table-responsive">
@@ -375,9 +376,9 @@
                 return this.account_info.abi.structs.filter(s => tables.find(t => t.name === s.name));
             },
             loadTableData (page) {
-                const pageSize = 10;
+                const pageSize = 2;
                 if (page < this.current_table.page) {
-                    this.current_table.upper_bound = this.current_table.lower_bound;
+                    this.current_table.upper_bound = this.current_table.lower_bound + 1;
                 }
                 if (page === 0) {
                     this.current_table.upper_bound = -1;
